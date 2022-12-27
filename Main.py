d8a32854164768b04e6ae04c55e29490dacce064
Gabriel-GCS <gabrielc-silva13@hotmail.com>
2022-12-27 02:10:38 -0300

    feat(user): createUser

---
+++ Main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from routes.UsuarioRoute import router as UsuarioRoute
+
+app = FastAPI()
+
+app.include_router(UsuarioRoute, tags=["Usuario"], prefix="/api/usuario")
+
+@app.get("/api/health", tags=["Health"])
+async def heath():
+    return{
+        "status":"ok"
+    }
